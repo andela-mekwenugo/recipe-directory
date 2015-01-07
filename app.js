26e86dba0dc1463b792aefac67b7a021bbd4a367
mirabelkoso <mirabelkoso@gmail.com>
2015-01-07 14:42:52 +0100

    using a dommy api

--- app.js
+++ app.js
@@ -1,2 +1,81 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
+app.use(bodyParser.urlencoded({extended:false}));
+
+// dommy data
+var recipesList = [
+{
+  "name":"salad",
+  "id" : "1",
+  "description" : "Made with carrot and cabbage"
+},
+
+{
+  "name" : "beans",
+  "id" : "2",
+  "description": "Very high in protein"
+},
+
+{
+  "name" : "stew",
+  "id" : "3",
+  "description" : "Mainly used to eat rice"
+},
+
+{
+  "name" : "pizza",
+  "id" : "4",
+  "description" : "Round bread garnished with vegetables and cheese"
+}, 
+
+{
+  "name" : "rice",
+  "id" : "5",
+  "description" : "Very high in protein"
+}]
+
+//created app router
+var router = express.Router();
+router
+  .get("/", function(request,response) {
+    response.status(200).json(recipesList);
+
+  })
+
+  .post("/", function(request, response) {
+    var newRecipe = request.body;
+    recipesList.push(newRecipe);
+    response.status(201).json(recipesList);
+
+  })
+
+  .put("/:id/edit", function(request, response) {
+    for(var i = 0; i < recipesList.length; i++) {
+      if (request.params.id === recipesList[i].id) {
+        recipesList[i].name = request.body.name;
+        recipesList[i].description = request.body.description;
+        break;
+      } else {
+        response.send("Not a recipe");
+      }
+    };
+    response.status(200).json(recipesList);
+  })
+
+  .delete("/:id/delete", function(request, response) {
+    for(var i = 0; i < recipesList.length; i++) {
+      if (request.params.id === recipesList[i].id) {
+        recipesList.splice(i,1);
+        break;
+      }
+    };
+      response.status(200).json(recipesList);
+  });
+
+app.use("/recipes", router);
+
+app.listen(419, function() {
+  console.log("Listening on port 419");
+});
