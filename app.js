17af6634373cc5023db21077faf011908b8a3c5d
mirabelkoso <mirabelkoso@gmail.com>
2015-01-08 16:07:22 +0100

    created a db using mongo and called several methods

--- app.js
+++ app.js
@@ -3,33 +3,16 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
 // //database connection to mongodb via mongoose
-// var dbURI = 'mongodb://localhost/recipeDirectory';
-// mongoose.connect(dbURI);
-// var db = mongoose.connection
-
-// //outputs message to console, depending on connection status
-// db.on('error', console.error.bind(console, 'connection error: '));
-// db.once('connected', function() {
-//   console.log('Mongoose connected to ' + dbURI);
-// })
-
-// //app database model
-// var recipesSchema = mongoose.Schema({
-//   name: String,
-//   Cuisine: String,
-//   Equipments: [String],
-//   ingredients: [String],
-//   method: [String]
-// });
 
 var app = express();
 app.use(bodyParser.urlencoded({extended:false}));
+// require('./models/db');
 
 
 // dommy data
 var routes = require('./routes/recipes');
-
 app.use("/recipes", routes);
+
 
 app.listen(3000, function() {
   console.log("Listening on port 3000");
--- routes/recipes.js
+++ routes/recipes.js
@@ -1,71 +1,98 @@
 var express = require('express');
-var recipesList = [
-{
-  "name":"salad",
-  "id" : "1",
-  "description" : "Made with carrot and cabbage"
-},
-
-{
-  "name" : "beans",
-  "id" : "2",
-  "description": "Very high in protein"
-},
-
-{
-  "name" : "stew",
-  "id" : "3",
-  "description" : "Mainly used to eat rice"
-},
-
-{
-  "name" : "pizza",
-  "id" : "4",
-  "description" : "Round bread garnished with vegetables and cheese"
-}, 
-
-{
-  "name" : "rice",
-  "id" : "5",
-  "description" : "Very high in protein"
-}]
+var mongoose = require('mongoose');
+require('../models/db');
+var Recipe = mongoose.model('Recipe');
 
 //created app router
 var router = express.Router();
 router
   .get("/", function(request,response) {
-    response.status(200).json(recipesList);
+    Recipe
+      .find(function(err, recipes) {
+        if(err) {
+          response.status(404).json(err);
+        }
+        response.json(recipes)
+      });
+      // .exec(function(err, recipes) {
+      //   if (err) {
+      //     response.status(404).json("can't get recipes");
+      //   }
+      //   response.status(200)
+      //   response.json(recipes)
+      // });
 
   })
 
-  .post("/", function(request, response) {
-    var newRecipe = request.body;
-    recipesList.push(newRecipe);
-    response.status(201).json(recipesList);
-
-  })
-
-  .put("/:id/edit", function(request, response) {
-    for(var i = 0; i < recipesList.length; i++) {
-      if (request.params.id === recipesList[i].id) {
-        recipesList[i].name = request.body.name;
-        recipesList[i].description = request.body.description;
-        break;
-      } else {
-        response.send("Not a recipe");
+  .post('/', function(request, response) {
+    var newRecipe = {
+        name: request.body.name,
+        cuisine: request.body.cuisine,
+        equipments: request.body.equipments,
+        ingredients: request.body.ingredients,
+        method: request.body.method
       }
-    };
-    response.status(200).json(recipesList);
+    Recipe.create(newRecipe, function(err, newRecipes) {
+      if(err) {
+        return handleError(err)
+      }
+      response.status(201).json(newRecipe)
+    })
   })
 
-  .delete("/:id/delete", function(request, response) {
-    for(var i = 0; i < recipesList.length; i++) {
-      if (request.params.id === recipesList[i].id) {
-        recipesList.splice(i,1);
-        break;
-      }
-    };
-      response.status(200).json(recipesList);
+  // .post("/", function(request, response) {
+  //   var newRecipe = request.body;
+  //   recipesList.push(newRecipe);
+  //   response.status(201).json(recipesList);
+
+  // })
+
+  .put('/edit/:id',function(request, response) {
+    Recipe
+      .findById(request.params.id)
+      .exec(function(err, recipes) {
+          recipes.name = request.body.name;
+          recipes.cuisine = request.body.cuisine;
+          recipes.equipments = request.body.equipments;
+          recipes.ingredients = request.body.ingredients;
+          recipes.method = request.body.method;
+          recipes.save();
+          response.status(200).json(recipes)
+      })
+  })
+
+  // .put("/:id/edit", function(request, response) {
+  //   for(var i = 0; i < recipesList.length; i++) {
+  //     if (request.params.id === recipesList[i].id) {
+  //       recipesList[i].name = request.body.name;
+  //       recipesList[i].description = request.body.description;
+  //       break;
+  //     } else {
+  //       response.send("Not a recipe");
+  //     }
+  //   };
+  //   response.status(200).json(recipesList);
+  // })
+
+
+  .delete('/delete/:id',function(request, response) {
+    Recipe
+      .findOneAndRemove({id:request.params.id}, function(err) {
+        if(err) {
+          response.send(err);
+        }
+        response.send("deleted!");
+      });
   });
+
+  // .delete("/:id/delete", function(request, response) {
+  //   for(var i = 0; i < recipesList.length; i++) {
+  //     if (request.params.id === recipesList[i].id) {
+  //       recipesList.splice(i,1);
+  //       break;
+  //     }
+  //   };
+  //     response.status(200).json(recipesList);
+  // });
 
   module.exports = router
